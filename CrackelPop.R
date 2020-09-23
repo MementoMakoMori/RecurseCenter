35a43c02b28b77152ce008a82d3b4a5c7bee1c0e
R. Holley <36553081+MementoMakoMori@users.noreply.github.com>
2020-09-23 12:48:52 -0400

    add cracklepop file

---
+++ CrackelPop.R
@@ -0,0 +1,23 @@
+## Recurse Center Application
+## R. Holley
+## Sept. 23, 2020
+
+## R version 3.6.3 (2020-02-29) -- "Holding the Windsock"
+
+## function to determine is numbers are divisible
+is.wholenumber <- function(x){
+  abs(x - round(x)) == 0}
+
+nums <- c(1:100)
+crack <- which(is.wholenumber(nums[]/3) & !is.wholenumber(nums[]/5))
+pop <- which(is.wholenumber(nums[]/5) & !is.wholenumber(nums[]/3))
+cp <- which(is.wholenumber(nums[]/3) & is.wholenumber(nums[]/5))
+
+nums[crack] <- "Crackle"
+nums[pop] <- "Pop"
+nums[cp] <- "CracklePop"
+
+print(nums)
+
+
+
